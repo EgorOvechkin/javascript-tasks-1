08e67810e8d680cce8b89ba4d925e4692068ad73
EgorOvechkin <egorka1391@gmail.com>
2015-10-08 12:04:12 +0500

    переменные перед if + немного code style

--- task1.js
+++ task1.js
@@ -1,29 +1,30 @@
+'use strict';
 var hours = process.argv[2];
 var minutes = process.argv[3];
 // Немного замечательного кода и магии
-if (minutes >= 0 && minutes <= 59 && hours >= 0 && hours <= 23)
 var forty = [];
 var nine = [];
 var four = [];
-var heightOfSymbol = 7;
+var HEIGHT_OF_SYMBOL = 7;
 var zero = ['     ','     ','     ','@@@@@','     ','     ','     '];
 var one = ['@@@@@ ','  @   ','  @   ','  @   ','  @   ','  @   ','@@@@@ '];
 var five = ['@       @ ','@       @ ','@       @ ',' @     @  ','  @   @   ','   @ @    ', '    @     '];
 var ten = ['@     @ ',' @   @  ','  @ @   ','   @    ','  @ @   ',' @   @  ','@     @ '];
 var fiveteen = ['@      ','@      ','@      ','@      ','@      ','@    @ ','@@@@@@ '];
 var colon = ['   ','   ',' @ ','   ',' @ ','   ','   '];
-{
-	for (var i = 0; i < heightOfSymbol; i++)
+var res = [];
+for (var i = 0; i < HEIGHT_OF_SYMBOL; i++)
 	{
 		forty[i] = ten[i] + fiveteen[i];
 		nine[i] = one[i] + ten[i];
 		four[i] = one[i] + five[i];
 	}
-	var res = [];
+if (minutes >= 0 && minutes <= 59 && hours >= 0 && hours <= 23)
+{
 	arabToRoman(hours);
 	res.push(colon);
 	arabToRoman(minutes);
-	for (var i = 0; i < heightOfSymbol; i++)
+	for (var i = 0; i < HEIGHT_OF_SYMBOL; i++)
 	{
 		var s = '';
 		for (var j = 0; j < res.length; j++)
@@ -58,8 +59,8 @@ function arabToRoman(num)
 {
 	var arabSymbol = [50, 40, 10, 9, 5, 4, 1, 0];
 	var romanSymbol = [fiveteen, forty, ten, nine, five, four, one, zero];
-	var	i = 0;
-	if (num == 0)
+	var i = 0;
+	if (num === 0)
 	{
 		res.push(romanSymbol[romanSymbol.length - 1]);
 	}
